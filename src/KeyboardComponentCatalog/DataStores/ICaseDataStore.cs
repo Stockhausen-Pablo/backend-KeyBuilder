3a9c5265ae0c1555c0b1d2c9c30559ab81b7026d
Pablo Stockhausen <stockhausen017@gmail.com>
2021-09-17 18:00:34 +0000

    base

---
+++ src/KeyboardComponentCatalog/DataStores/ICaseDataStore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using KeyboardComponentCatalog.Models;
+
+namespace KeyboardComponentCatalog.DataStores
+{
+    public interface ICaseDataStore
+    {
+        [ItemNotNull]
+        public Task<Case> GetCaseById(long id);
+
+        [ItemNotNull]
+        public Task<ICollection<Case>> GetAllCases();
+
+        public Task RemoveCaseById(long id);
+    }
+}
